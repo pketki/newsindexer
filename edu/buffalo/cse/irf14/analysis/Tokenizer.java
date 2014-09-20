a92732853abf7c5c6ac58fc25ee83645cecb9dec
pketki <ketki.purandare23@gmail.com>
2014-09-20 12:06:49 -0400

    Tokenizer basic changes

--- edu/buffalo/cse/irf14/analysis/Tokenizer.java
+++ edu/buffalo/cse/irf14/analysis/Tokenizer.java
@@ -3,42 +3,50 @@
  */
 package edu.buffalo.cse.irf14.analysis;
 
+import java.util.Scanner;
+
 /**
- * @author nikhillo
- * Class that converts a given string into a {@link TokenStream} instance
+ * @author nikhillo Class that converts a given string into a
+ *         {@link TokenStream} instance
  */
 public class Tokenizer {
+
+	private Scanner tokenReader;
+
 	/**
 	 * Default constructor. Assumes tokens are whitespace delimited
 	 */
 	public Tokenizer() {
-		//TODO : YOU MUST IMPLEMENT THIS METHOD
+		tokenReader = new Scanner("");
 	}
-	
+
 	/**
 	 * Overloaded constructor. Creates the tokenizer with the given delimiter
-	 * @param delim : The delimiter to be used
+	 * 
+	 * @param delim
+	 *            : The delimiter to be used
 	 */
 	public Tokenizer(String delim) {
-		//TODO : YOU MUST IMPLEMENT THIS METHOD
+		tokenReader.useDelimiter(delim);
 	}
-	
+
 	/**
-	 * Method to convert the given string into a TokenStream instance.
-	 * This must only break it into tokens and initialize the stream.
-	 * No other processing must be performed. Also the number of tokens
-	 * would be determined by the string and the delimiter.
-	 * So if the string were "hello world" with a whitespace delimited
-	 * tokenizer, you would get two tokens in the stream. But for the same
-	 * text used with lets say "~" as a delimiter would return just one
-	 * token in the stream.
-	 * @param str : The string to be consumed
+	 * Method to convert the given string into a TokenStream instance. This must
+	 * only break it into tokens and initialize the stream. No other processing
+	 * must be performed. Also the number of tokens would be determined by the
+	 * string and the delimiter. So if the string were "hello world" with a
+	 * whitespace delimited tokenizer, you would get two tokens in the stream.
+	 * But for the same text used with lets say "~" as a delimiter would return
+	 * just one token in the stream.
+	 * 
+	 * @param str
+	 *            : The string to be consumed
 	 * @return : The converted TokenStream as defined above
-	 * @throws TokenizerException : In case any exception occurs during
-	 * tokenization
+	 * @throws TokenizerException
+	 *             : In case any exception occurs during tokenization
 	 */
 	public TokenStream consume(String str) throws TokenizerException {
-		//TODO : YOU MUST IMPLEMENT THIS METHOD
+		// TODO : YOU MUST IMPLEMENT THIS METHOD
 		return null;
 	}
 }
--- edu/buffalo/cse/irf14/document/Parser.java
+++ edu/buffalo/cse/irf14/document/Parser.java
@@ -9,30 +9,33 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * @author nikhillo
- * Class that parses a given file into a Document
+ * @author nikhillo Class that parses a given file into a Document
  */
 public class Parser {
 	/**
 	 * Static method to parse the given file into the Document object
-	 * @param filename : The fully qualified filename to be parsed
+	 * 
+	 * @param filename
+	 *            : The fully qualified filename to be parsed
 	 * @return The parsed and fully loaded Document object
-	 * @throws ParserException In case any error occurs during parsing
+	 * @throws ParserException
+	 *             In case any error occurs during parsing
 	 */
 	public static Document parse(String filename) throws ParserException {
 
 		final Document document = new Document();
 
-		String [] path = filename.split("\\\\");
-		document.setField(FieldNames.FILEID, path[path.length-1]);
-		document.setField(FieldNames.CATEGORY, path[path.length-2]);
+		String[] path = filename.split("\\\\");
+		document.setField(FieldNames.FILEID, path[path.length - 1]);
+		document.setField(FieldNames.CATEGORY, path[path.length - 2]);
 
-		Pattern month = Pattern.compile("\\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\D*");
-		//String titleRegEx = "^[A-Z0-9<>()/.,'\\-\\s]*$";
+		Pattern month = Pattern
+				.compile("\\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\D*");
+		// String titleRegEx = "^[A-Z0-9<>()/.,'\\-\\s]*$";
 		String line, authorName, authorOrg = null;
 		StringBuffer place = new StringBuffer("");
 		StringBuffer content = new StringBuffer("");
-		
+
 		try {
 			Scanner reader = new Scanner(new File(filename));
 			while (reader.hasNextLine()) {
@@ -44,11 +47,12 @@ public class Parser {
 
 				if (document.getField(FieldNames.TITLE) == null) {
 					document.setField(FieldNames.TITLE, line);
-				
-					//check for author info
+
+					// check for author info
 					if (reader.findWithinHorizon("<AUTHOR>", 200) != null) {
 						reader.findInLine("by|By|BY");
-						String[] authorInfo = reader.nextLine().split(",|</AUTHOR>");
+						String[] authorInfo = reader.nextLine().split(
+								",|</AUTHOR>");
 						authorName = authorInfo[0];
 						if (authorInfo.length > 1) {
 							authorOrg = authorInfo[1];
@@ -57,11 +61,12 @@ public class Parser {
 						document.setField(FieldNames.AUTHORORG, authorOrg);
 					}
 
-					//capture place till date token arrives
+					// capture place till date token arrives
 					while (!reader.hasNext(month) && reader.hasNext()) {
 						place.append(reader.next());
 					}
-					//if a file is unformatted and has no date put everything in content
+					// if a file is unformatted and has no date put everything
+					// in content
 					if (!reader.hasNext()) {
 						document.setField(FieldNames.CONTENT, place.toString());
 						break;
@@ -73,14 +78,14 @@ public class Parser {
 
 				} else {
 					content.append(line + " ");
-				}				
+				}
 			}
 			document.setField(FieldNames.CONTENT, content.toString());
-			
+			reader.close();
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
 		return document;
 	}
 
