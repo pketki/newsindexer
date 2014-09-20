e9b5207d78b4e1aebf1731ce2d8bed6c368d0be6
jlimaye <jitendralimaye@gmail.com>
2014-09-20 18:57:02 -0400

    date filter

--- edu/buffalo/cse/irf14/analysis/rules/DateFilter.java
+++ edu/buffalo/cse/irf14/analysis/rules/DateFilter.java
@@ -3,6 +3,10 @@
  */
 package edu.buffalo.cse.irf14.analysis.rules;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import edu.buffalo.cse.irf14.analysis.Token;
 import edu.buffalo.cse.irf14.analysis.TokenFilter;
 import edu.buffalo.cse.irf14.analysis.TokenStream;
@@ -13,13 +17,14 @@ import edu.buffalo.cse.irf14.analysis.TokenizerException;
  * 
  */
 public class DateFilter extends TokenFilter {
+	private final Calendar calendar;
 
 	/**
 	 * @param stream
 	 */
 	public DateFilter(TokenStream stream) {
 		super(stream);
-
+		calendar = new GregorianCalendar();
 	}
 
 	/*
@@ -32,6 +37,27 @@ public class DateFilter extends TokenFilter {
 		if (getStream().hasNext()) {
 			Token token = getStream().next();
 			if (token != null) {
+				// Pattern pattern = Pattern
+				// .compile("[(\\d)(\\s)]*\\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\D*[(\\s)(\\d),]*(\\d{2,4})*");
+				// Matcher match = pattern.matcher(token.getTermText());
+				// if (match.find()) {
+				//
+				// }
+				if (token.getTermText().matches("\\d{4}")) {
+					calendar.set(Calendar.ERA, GregorianCalendar.AD);
+					calendar.set(Integer.parseInt(token.getTermText()), 1, 1,
+							0, 0, 0);
+				}
+				if (token.getTermText().matches("\\d{1,4}(\\s)*(BC)")) {
+					String year = token.getTermText().split("BC")[0].toString()
+							.trim();
+					calendar.set(Calendar.ERA, GregorianCalendar.BC);
+					calendar.set(Integer.parseInt(year), 1, 1, 0, 0, 0);
+				}
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+						"yyyyMMdd");
+				String dateString = simpleDateFormat.format(calendar.getTime());
+				token.setTermText(dateString);
 				return true;
 			}
 		}
