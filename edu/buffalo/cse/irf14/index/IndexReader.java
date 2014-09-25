5ddbcf51fc68643ab40878ab4d5abc5fefb8e1ed
jlimaye <jitendralimaye@gmail.com>
2014-09-25 13:19:22 -0400

    index reader initial

--- edu/buffalo/cse/irf14/index/IndexReader.java
+++ edu/buffalo/cse/irf14/index/IndexReader.java
@@ -3,82 +3,129 @@
  */
 package edu.buffalo.cse.irf14.index;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * @author nikhillo
- * Class that emulates reading data back from a written index
+ * @author nikhillo Class that emulates reading data back from a written index
  */
 public class IndexReader {
+	private final String indexDir;
+	private final IndexType type;
+	private FileInputStream fileInputStream;
+	private ObjectInputStream objectInputStream;
+	Map<String, Integer> termsMap;
+
 	/**
 	 * Default constructor
-	 * @param indexDir : The root directory from which the index is to be read.
-	 * This will be exactly the same directory as passed on IndexWriter. In case 
-	 * you make subdirectories etc., you will have to handle it accordingly.
-	 * @param type The {@link IndexType} to read from
+	 * 
+	 * @param indexDir
+	 *            : The root directory from which the index is to be read. This
+	 *            will be exactly the same directory as passed on IndexWriter.
+	 *            In case you make subdirectories etc., you will have to handle
+	 *            it accordingly.
+	 * @param type
+	 *            The {@link IndexType} to read from
 	 */
 	public IndexReader(String indexDir, IndexType type) {
-		//TODO
+		this.indexDir = indexDir;
+		this.type = type;
+		try {
+			File fileDir = new File(indexDir);
+			File[] fileList = fileDir.listFiles();
+			for (int i = 0; i < fileList.length; i++) {
+				fileInputStream = new FileInputStream(fileList[i]);
+				if (fileInputStream != null) {
+					objectInputStream = new ObjectInputStream(fileInputStream);
+					termsMap = new HashMap<String, Integer>();
+					termsMap.putAll((Map<String, Integer>) objectInputStream
+							.readObject());
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
-	 * Get total number of terms from the "key" dictionary associated with this 
+	 * Get total number of terms from the "key" dictionary associated with this
 	 * index. A postings list is always created against the "key" dictionary
+	 * 
 	 * @return The total number of terms
 	 */
 	public int getTotalKeyTerms() {
-		//TODO : YOU MUST IMPLEMENT THIS
+		// TODO : YOU MUST IMPLEMENT THIS
 		return -1;
 	}
-	
+
 	/**
-	 * Get total number of terms from the "value" dictionary associated with this 
-	 * index. A postings list is always created with the "value" dictionary
+	 * Get total number of terms from the "value" dictionary associated with
+	 * this index. A postings list is always created with the "value" dictionary
+	 * 
 	 * @return The total number of terms
 	 */
 	public int getTotalValueTerms() {
-		//TODO: YOU MUST IMPLEMENT THIS
+		// TODO: YOU MUST IMPLEMENT THIS
 		return -1;
 	}
-	
+
 	/**
-	 * Method to get the postings for a given term. You can assume that
-	 * the raw string that is used to query would be passed through the same
-	 * Analyzer as the original field would have been.
-	 * @param term : The "analyzed" term to get postings for
-	 * @return A Map containing the corresponding fileid as the key and the 
-	 * number of occurrences as values if the given term was found, null otherwise.
+	 * Method to get the postings for a given term. You can assume that the raw
+	 * string that is used to query would be passed through the same Analyzer as
+	 * the original field would have been.
+	 * 
+	 * @param term
+	 *            : The "analyzed" term to get postings for
+	 * @return A Map containing the corresponding fileid as the key and the
+	 *         number of occurrences as values if the given term was found, null
+	 *         otherwise.
 	 */
 	public Map<String, Integer> getPostings(String term) {
-		//TODO:YOU MUST IMPLEMENT THIS
+
 		return null;
 	}
-	
+
 	/**
 	 * Method to get the top k terms from the index in terms of the total number
 	 * of occurrences.
-	 * @param k : The number of terms to fetch
-	 * @return : An ordered list of results. Must be <=k fr valid k values
-	 * null for invalid k values
+	 * 
+	 * @param k
+	 *            : The number of terms to fetch
+	 * @return : An ordered list of results. Must be <=k fr valid k values null
+	 *         for invalid k values
 	 */
 	public List<String> getTopK(int k) {
-		//TODO YOU MUST IMPLEMENT THIS
+		// TODO YOU MUST IMPLEMENT THIS
 		return null;
 	}
-	
+
 	/**
 	 * Method to implement a simple boolean AND query on the given index
-	 * @param terms The ordered set of terms to AND, similar to getPostings()
-	 * the terms would be passed through the necessary Analyzer.
-	 * @return A Map (if all terms are found) containing FileId as the key 
-	 * and number of occurrences as the value, the number of occurrences 
-	 * would be the sum of occurrences for each participating term. return null
-	 * if the given term list returns no results
-	 * BONUS ONLY
+	 * 
+	 * @param terms
+	 *            The ordered set of terms to AND, similar to getPostings() the
+	 *            terms would be passed through the necessary Analyzer.
+	 * @return A Map (if all terms are found) containing FileId as the key and
+	 *         number of occurrences as the value, the number of occurrences
+	 *         would be the sum of occurrences for each participating term.
+	 *         return null if the given term list returns no results BONUS ONLY
 	 */
-	public Map<String, Integer> query(String...terms) {
-		//TODO : BONUS ONLY
+	public Map<String, Integer> query(String... terms) {
+		// TODO : BONUS ONLY
 		return null;
 	}
 }
